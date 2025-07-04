9ab7bca201ca1c5a635722a5ff3ce53140fbe4fe
Ilya ZoolooS Popov <zooloosik@gmail.com>
2025-07-04 20:46:48 +0300

    Better error handling and messaging.

--- main.py
+++ main.py
@@ -17,9 +17,11 @@ from settings import (
 MSG = {
     'ERR_FILE': 'Something wrong with file {filename}.',
     'ERR_XML': 'Invalid XML format in {filename}.',
-    'ERR_PATH': 'Can\'t write to {filename}. Check path.',
+    'ERR_IN_PATH': 'Can\'t read from {filename}. Check path.',
+    'ERR_OUT_PATH': 'Can\'t write to {filename}. Check path.',
     'ERR_UNKNOWN': 'Something went wrong.',
-    'GOOD': 'You can check result.'
+    'GOOD': 'You can check {filename}.',
+    'END': 'All processes has ended.'
 }
 
 
@@ -28,6 +30,10 @@ def get_mods_data(filename):
         tree = etree.parse(filename)
     except IOError:
         print(MSG['ERR_FILE'].format(filename=filename))
+        return
+    except (FileNotFoundError, UnboundLocalError):
+        print(MSG['ERR_IN_PATH'].format(filename=filename))
+        return
     except etree.XMLSyntaxError:
         print(MSG['ERR_XML'].format(filename=filename))
     except Exception:
@@ -46,21 +52,37 @@ def put_to_txt(filename, payload, mode='w'):
         with open(filename, mode, encoding='utf-8') as f:
             f.write(payload)
     except FileNotFoundError:
-        print(MSG['ERR_PATH'].format(filename=filename))
+        print(MSG['ERR_OUT_PATH'].format(filename=filename))
         raise
 
 
 def main():
     for f in SRC_FILES:
-        path_out = Path(PATH_OUT, f[:-4] + TARGET_SUFFIX + TARGET_EXT)
+        if not Path(PATH_IN).is_dir():
+            print(MSG['ERR_IN_PATH'].format(filename=PATH_IN))
+            break
+
+        curr_f_path_in = Path(PATH_IN, f)
+        if not curr_f_path_in.is_file():
+            print(MSG['ERR_IN_PATH'].format(filename=f))
+            continue
+
+        if not Path(PATH_OUT).is_dir():
+            Path(PATH_OUT).mkdir()
+
+        curr_f_path_out = Path(PATH_OUT, f[:-4] + TARGET_SUFFIX + TARGET_EXT)
+
         try:
-            put_to_txt(path_out, '')
+            put_to_txt(curr_f_path_out, '')  # clear file if exists or create empty file
         except FileNotFoundError:
             return
-        for line in get_mods_data(Path(PATH_IN, f)):
-            put_to_txt(path_out, line, 'a')
 
-    print(MSG['GOOD'])
+        for line in get_mods_data(curr_f_path_in):
+            put_to_txt(curr_f_path_out, line, mode='a')
+
+        print(MSG['GOOD'].format(filename=curr_f_path_out))
+
+    print(MSG['END'])
 
 
 if __name__ == '__main__':
